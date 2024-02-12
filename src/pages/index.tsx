b1a557e1bb318008c242c6f6e1f27907ada8e3d4
Corey J <cjw.rutt@gmail.com>
2024-02-11 19:11:03 -0800

    WIP: Header component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,11 @@
 import Label from "@/components/label";
+import Header from "@/components/header";
 
 export default function Home(){
 
   return (
-    <div>
+    <div className="body">
+      <Header />
       <h1>PokemonFormula</h1>
       <Label title="Testing"/>
     </div>
