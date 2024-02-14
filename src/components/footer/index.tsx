7897da6fc329595f02fd33ab942b11a700a3548c
zzs1 <498150092z@gmail.com>
2024-02-13 20:33:45 -0800

    Footer Component(Minor Fix required)

---
+++ src/components/footer/index.tsx
@@ -0,0 +1,27 @@
+import React, { useState, useEffect } from 'react';
+import styles from './Footer.module.css';
+
+export default function Footer() {
+   
+    return (
+        <div className={styles.footerOuterContainer}>
+            <div className={styles.footerContainer}>
+            <div className={styles.logoRow}>
+                    <img src='../../images/logo.png' alt="Logo" className={styles.logo} />
+                    <h1>PokéFormula</h1>
+                    <p>ALl rights Reserved 2024</p>
+                </div>
+                    <div className={styles.menuItems}>
+                        <div className={styles.menudirection}>
+                        <p>Home</p>
+                        <p>Battle</p>
+                        <p>Dex</p>
+                        </div>
+                    </div>
+            </div>
+        </div>
+
+    );
+
+
+}
