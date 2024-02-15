dbfc3794ffddf0d4467253a7e1af3163ff39c7be
ReallyNguyen <jnguyen188@my.bcit.ca>
2024-02-14 18:40:03 -0800

    improved footer.

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -1,34 +1,30 @@
 import React, { useState, useEffect } from 'react';
+import Link from 'next/link';
 import styles from './Footer.module.css';
 
 export default function Footer() {
    
     return (
-        <div className={styles.footerOuterContainer}>
-            <div className={styles.footerContainer}>
-            <div className={styles.logoRow}>
-                    <div className={styles.logoLine}>
+        <div className={styles.container}>
+            <div className={styles.left}>
+                <div className={styles.logoContainer}>
                     <img src='../../images/logo.png' alt="Logo" className={styles.logo} />
-                    <h1>PokéFormula</h1>
-                    </div>
-                    <p>ALl rights Reserved 2024</p>
-
-                    </div>
-                        <div className={styles.menuItems}>
-                    <img src='../../images/logo.png' alt="Logo" className={styles.logo} />
-                    <h1>PokéFormula</h1>
-                    <p>ALl rights Reserved 2024</p>
+                    <h1>PokeFormula</h1>
                 </div>
-                    <div className={styles.menuItems}>
-                        <div className={styles.menudirection}>
-                        <p>Home</p>
-                        <p>Battle</p>
-                        <p>Dex</p>
-                        </div>
-                </div>
-
-                    </div>
+                <h2>All rights reserved? 2024</h2>
             </div>
+            <div className={styles.right}>
+                <Link href={""}>
+                    <h2>Battle</h2>
+                </Link>
+                <Link href={""}>
+                    <h2>PokeDex</h2>
+                </Link>
+                <Link href={""}>
+                    <h2>Account</h2>
+                </Link>
+            </div>
+            
         </div>
 
     );
