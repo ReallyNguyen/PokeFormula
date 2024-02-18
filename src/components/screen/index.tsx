ce24e9fefa8caa6bfac8c50218260d14db789493
ReallyNguyen <jnguyen188@my.bcit.ca>
2024-02-18 12:31:03 -0800

    3d screen (not responsive)

--- src/components/screen/index.tsx
+++ src/components/screen/index.tsx
@@ -4,21 +4,27 @@ interface IScreen {
     [key: string]: any
 }
 
-export default function Screen({title}: IScreen){
-    return(
+export default function Screen({ title }: IScreen) {
+    return (
         <>
-            <div className={styles.container}>
-                <div className={styles.circleContainer}>
-                    <div className={styles.circle}></div>
-                    <div className={styles.circle}></div>
+        <div className={styles.bigcontainer}>
+            <div className={styles.testing}>
+                <div className={styles.top}></div>
+                <div className={styles.container}>
+                    <div className={styles.circleContainer}>
+                        <div className={styles.circle}></div>
+                        <div className={styles.circle}></div>
+                    </div>
+                    <div className={styles.innerContainer}>
+                        {title}
+                    </div>
+                    <div className={styles.bigCircle}></div>
                 </div>
-                <div className={styles.innerContainer}>
-                    <h1>{title}</h1>
-                </div>
-                <div className={styles.bigCircle}></div>
+                
             </div>
-            
+            <div className={styles.right}></div>
+        </div>
+    
         </>
-
     )
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -16,6 +16,7 @@ export default function Home(){
       <Label title="Testing"/>
       <Footer /> */}
       <Screen title='testing'/>
+
     </div>
   )
 
