b41955561308be9c0433327d81ca40418575106b
ReallyNguyen <jnguyen188@my.bcit.ca>
2024-02-17 21:45:32 -0800

    screen component without 3d

--- src/components/screen/index.tsx
+++ src/components/screen/index.tsx
@@ -6,8 +6,19 @@ interface IScreen {
 
 export default function Screen({title}: IScreen){
     return(
-        <div className={styles.container}>
-            {title}
-        </div>
+        <>
+            <div className={styles.container}>
+                <div className={styles.circleContainer}>
+                    <div className={styles.circle}></div>
+                    <div className={styles.circle}></div>
+                </div>
+                <div className={styles.innerContainer}>
+                    <h1>{title}</h1>
+                </div>
+                <div className={styles.bigCircle}></div>
+            </div>
+            
+        </>
+
     )
 }
