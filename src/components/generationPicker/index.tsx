931d9f8f5099fdba532758d3b5fd9700448147d5
Corey J <cjw.rutt@gmail.com>
2024-02-19 16:09:35 -0800

    Pokemon DD Select by Gen Working

--- src/components/generationPicker/index.tsx
+++ src/components/generationPicker/index.tsx
@@ -4,31 +4,33 @@ import axios from 'axios';
 export default function GenerationPicker({ setSelectedGen }: GenerationPickerProps) {
     const [generations, setGenerations] = useState<IGeneration[]>([]);
   
-    useEffect(() => {
-      const fetchGenerations = async () => {
-        try {
-          const response = await axios.get('https://pokeapi.co/api/v2/generation/');
-          const { results } = response.data;
+	useEffect(() => {
+		const fetchGenerations = async () => {
+			try {
+				const response = await axios.get('https://pokeapi.co/api/v2/generation/');
+				const { results } = response.data;
+
+				setGenerations(results);
+			} catch (error) {
+				console.error('Error fetching generations:', error);
+			}
+		};
+
+		fetchGenerations();
+	}, []);
   
-          setGenerations(results);
-        } catch (error) {
-          console.error('Error fetching generations:', error);
-        }
-      };
-  
-      fetchGenerations();
-    }, []);
-  
-    return (
-      <div>
-        <select onChange={(e) => setSelectedGen(e.target.value)}>
-          <option value="">Select Generation...</option>
-          {generations.map((generation) => (
-            <option key={generation.name} value={generation.url}>
-              {generation.name}
-            </option>
-          ))}
-        </select>
-      </div>
-    );
+	return (
+		<div>
+			<select onChange={(e) => setSelectedGen(e.target.value)}>
+				<option value="">Select Generation...</option>
+				{
+					generations.map((generation) => (
+						<option key={generation.name} value={generation.url}>
+							{generation.name}
+						</option>
+					))
+				}
+			</select>
+		</div>
+	);
   }
