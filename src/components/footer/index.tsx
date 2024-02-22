21ab050f6b3586e08525a3769d016b9f00fac1a5
zzs1 <498150092z@gmail.com>
2024-02-22 00:54:19 -0800

    merge

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -7,31 +7,6 @@ export default function Footer() {
     return (
         <footer className={styles.footer}>
             <div className={styles.footerContainer}>
-<<<<<<< HEAD
-            <div className={styles.logoRow}>
-                    <div className={styles.logoLine}>
-                    <img src='../../images/logo.png' alt="Logo" className={styles.logo} />
-
-                    <h1 className={styles.logoTitle}>PokéFormula</h1>
-                    <div className={styles.menudirection}>
-                        <p>Home</p>
-                        <p>Battle</p>
-                        <p>PokéDex</p>
-                        </div>
-                    </div>
-
-                    <div className={styles.marks}>
-                    <p>ALl rights Reserved 2024</p>
-                    </div>
-
-            </div>
-            <div className={styles.menuItems}>
-
-                    </div>
-
-            </div>
-        </div>
-=======
                 <div className={styles.footerTop}>
                     <div className={styles.left}>
                         <div className={styles.logoContainer}>
@@ -54,7 +29,6 @@ export default function Footer() {
                 <p className={styles.copyright}>All rights reserved. Original Materials Copyright 2024. All other copyrights held by Nintendo 2024. ©</p>
             </div>
         </footer>
->>>>>>> 48ca806f865327b028ab80bd05720d85e80ee47f
     );
 
 
