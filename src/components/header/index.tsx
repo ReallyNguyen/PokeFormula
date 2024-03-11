5113c4f7778fee4b922757101a834a3d314a0669
ReallyNguyen <jnguyen188@my.bcit.ca>
2024-03-10 18:17:09 -0700

    improved header

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,11 +1,10 @@
 import { useState, useEffect } from 'react';
 import { useRouter } from 'next/router';
-
 import styles from './Header.module.css';
-
 
 export default function Header() {
     const [isMobile, setIsMobile] = useState(false);
+    const [menuOpen, setMenuOpen] = useState(false); // Track menu state
     const router = useRouter();
 
     const handleNavigation = (path: string) => {
@@ -24,19 +23,22 @@ export default function Header() {
         return () => window.removeEventListener('resize', handleResize);
     }, []);
 
+    const toggleMenu = () => {
+        setMenuOpen(!menuOpen);
+    };
 
     return (
         <header className={styles.headerContainer}>
             <div className={styles.headerInnerContainer}>
-                    <div>
-                        <a href='/' className={styles.logoRow}>
-                            <img src='../../images/logo.png' alt="Logo" className={styles.logo} />
-                            <h1 className={styles.h1}>PokéFormula</h1>          
-                        </a>
-                    </div>
+                <div>
+                    <a href='/' className={styles.logoRow}>
+                        <img src='../../images/logo.png' alt="Logo" className={styles.logo} />
+                        <h1 className={styles.h1}>PokéFormula</h1>          
+                    </a>
+                </div>
                 {isMobile ? (
-                    <div className={styles.mobileMenuIcon}>
-                       
+                    <div className={styles.mobileMenuIcon} onClick={toggleMenu}>
+                        <img src={'../../images/menu.svg'} width={30} height={30} alt='menu' />
                     </div>
                 ) : (
                     <div className={styles.menuItems}>
@@ -47,6 +49,20 @@ export default function Header() {
                     </div>
                 )}
             </div>
+            {/* Render menu items conditionally based on menuOpen state */}
+            {isMobile && menuOpen && (
+                <div className={styles.mobileMenu}>
+                    <div className={styles.closeIcon} onClick={toggleMenu}>
+                        <img src={'../../images/close.svg'} width={20} height={20} alt='close' />
+                    </div>
+                    <div className={styles.mobileMenuItems}>
+                        <a href='/' onClick={toggleMenu}>Home</a>
+                        <a href='/battle' onClick={toggleMenu}>Battle</a>
+                        <a href='/dex' onClick={toggleMenu}>PokéDex</a>
+                        <a href='/profile' onClick={toggleMenu}>Account</a>
+                    </div>
+                </div>
+            )}
         </header>
     );
 }
