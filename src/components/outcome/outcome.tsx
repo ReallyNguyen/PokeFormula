049dabe5ce65cbaf50de74e2fac282a82ea5e607
ReallyNguyen <jnguyen188@my.bcit.ca>
2024-03-21 19:37:29 -0700

    modal responsive and header

--- src/components/outcome/outcome.tsx
+++ src/components/outcome/outcome.tsx
@@ -9,9 +9,8 @@ export default function Outcome({outcome, func1, func2, exit, button1, button2}:
         {outcome === 'win' ? (
           <div className={styles.container}>
             <button className={styles.closeButton} onClick={exit}>X</button>
-            <h1>Victory!</h1>
-            <div className={styles.line} />
-            <h3>You gained a TCG card!</h3>
+            <h1 className={styles.h1}>Victory!</h1>
+            <h3 className={styles.h3}>You gained a TCG card!</h3>
             <Tcg />
             <div className={styles.buttonContainer}>
                 <Button type="outcome" func={func1} name={button1} link=''/>
@@ -21,10 +20,9 @@ export default function Outcome({outcome, func1, func2, exit, button1, button2}:
         ) : outcome === 'lose' ? (
           <div className={styles.container}>
             <button className={styles.closeButton} onClick={exit}>X</button>
-            <h1>Defeated!</h1>
-            <div className={styles.line} />
-            <h3>Your Pokémon Fainted!</h3>
-            <Image src='../../images/Lose.svg' width={300} height={400} alt={"Defeat"} />
+            <h1 className={styles.h1}>Defeated!</h1>
+            <h3 className={styles.h3}>Your Pokémon Fainted!</h3>
+            <Image src='../../images/Lose.svg' width={150} height={300} alt={"Defeat"} />
             <div className={styles.buttonContainer}>
                 <Button type="outcome" func={func1} name={button1} link=''/>
                 <Button type="outcome" func={func2} name={button2} link=''/>           
